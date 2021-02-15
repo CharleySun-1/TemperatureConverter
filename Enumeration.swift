07c451416d3b040ea7994d367bcebeb3a9eae500
CharleySun <Ysun@lcs.on.ca>
2021-02-15 15:51:01 -0500

    Starting to change the program into an app.

---
+++ Enumeration.swift
@@ -0,0 +1,8 @@
+//
+//  Enumeration.swift
+//  TemperatureConverter
+//
+//  Created by Charley Sun on 2021-02-15.
+//
+
+import Foundation
---
+++ TemperatureConverterApp/TemperatureConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterAppApp.swift
+//  TemperatureConverterApp
+//
+//  Created by Charley Sun on 2021-02-15.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
